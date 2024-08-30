6190ba0e9c1c6beed78a7ceb5a93f3c79badb103
marcusryden92 <marcus.ryden1992@gmail.com>
2024-08-30 22:25:27 +0200

    Changed useCurrentUser to currentUser in Settings

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,11 +1,10 @@
 import { Card, CardHeader, CardContent } from "@/components/ui/card";
-import { useCurrentUser } from "@/hooks/use-current-user";
-
+import { currentUser } from "@/lib/auth";
 import SettingsForm from "./_components/settings-form";
 import { SettingsPageUser } from "@/next-auth";
 
 const SettingsPage = async () => {
-  const user = (await useCurrentUser()) as SettingsPageUser;
+  const user = (await currentUser()) as SettingsPageUser;
 
   return (
     <Card className="w-[600px]">
