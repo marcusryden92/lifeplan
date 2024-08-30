c31267b0c10568c5338f9956b4afe19f3e64c668
marcusryden92 <marcus.ryden1992@gmail.com>
2024-08-30 19:56:38 +0200

    Added loading page for Settings.

---
+++ app/(protected)/settings/loading.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BarLoader } from "react-spinners";
+
+const Loading = () => (
+  <div className="flex items-center justify-center min-h-screen">
+    <BarLoader />
+  </div>
+);
+
+export default Loading;
