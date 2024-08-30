e0592a2f031e279acbd8db7182dd9bef61225baa
marcusryden92 <marcus.ryden1992@gmail.com>
2024-08-30 21:29:07 +0200

    Updated SettingsPageUser

--- app/(protected)/settings/layout.tsx
+++ app/(protected)/settings/layout.tsx
@@ -2,10 +2,16 @@ import React, { Suspense } from "react";
 import SettingsPage from "./page";
 import Loading from "./loading";
 import { currentUser } from "@/lib/auth";
-import { User } from "@/next-auth";
+import { SettingsPageUser } from "@/next-auth";
 
-export default async function Layout() {
-  const user = (await currentUser()) as User;
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = (await currentUser()) as SettingsPageUser;
+
+  console.log(user);
 
   return (
     <div>
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -34,9 +34,9 @@ import { useCurrentUser } from "@/hooks/use-current-user";
 import { FormSuccess } from "@/components/ui/form-success";
 import { FormError } from "@/components/ui/form-error";
 import { UserRole } from "@prisma/client";
-import { ExtendedUser } from "@/next-auth";
+import { SettingsPageUser } from "@/next-auth";
 
-const SettingsPage = ({ user }: { user: ExtendedUser }) => {
+const SettingsPage = ({ user }: { user: SettingsPageUser }) => {
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
 
