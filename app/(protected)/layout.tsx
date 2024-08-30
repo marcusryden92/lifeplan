e6f5b37b191bd36c50e9269054982cbc58d539cc
marcusryden92 <marcus.ryden1992@gmail.com>
2024-08-30 22:41:08 +0200

    updated layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -8,7 +8,7 @@ const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
   // bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-orange-200 to-orange-500
 
   return (
-    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center">
+    <div className="h-full w-full flex gap-y-10 items-center justify-center">
       <Navbar />
       {children}
     </div>
