b593de95f6924d7e713485e9de993e1497136ede
marcusryden92 <marcus.ryden1992@gmail.com>
2024-09-02 04:24:07 +0200

    //

--- app/(protected)/create/page.tsx
+++ app/(protected)/create/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { CardHeader, CardContent } from "@/components/ui/card";
-import { CardWrapper } from "@/components/auth/card-wrapper";
 import {
   FormField,
   Form,
@@ -17,15 +16,18 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { TaskListSchema } from "@/schemas";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { PencilIcon } from "@heroicons/react/24/outline";
+import { TrashIcon } from "@heroicons/react/24/outline";
 
 export default function CreatePage() {
   const [taskArray, setTaskArray] = useState<z.infer<typeof TaskListSchema>[]>(
     []
   );
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+  const [editTitle, setEditTitle] = useState<string>("");
 
   const form = useForm<z.infer<typeof TaskListSchema>>({
     resolver: zodResolver(TaskListSchema),
@@ -34,25 +36,75 @@ export default function CreatePage() {
     },
   });
 
+  const tasksContainerRef = useRef<HTMLDivElement>(null);
+  const prevTaskLengthRef = useRef(taskArray.length);
+
   const onSubmit = (values: z.infer<typeof TaskListSchema>) => {
-    setTaskArray((prevTasks) => [...prevTasks, values]);
+    if (editIndex !== null) {
+      setTaskArray((prevTasks) =>
+        prevTasks.map((task, index) =>
+          index === editIndex ? { ...task, title: values.title } : task
+        )
+      );
+      setEditIndex(null);
+      setEditTitle("");
+    } else {
+      setTaskArray((prevTasks) => [...prevTasks, values]);
+    }
     form.reset(); // Reset form after submission
   };
 
   const deleteTask = (index: number) => {
     setTaskArray((prevTasks) => prevTasks.filter((_, i) => i !== index));
+    if (editIndex === index) {
+      setEditIndex(null);
+      setEditTitle("");
+    }
   };
+
+  const deleteAll = () => {
+    setTaskArray([]);
+  };
+
+  const handleEditClick = (index: number) => {
+    setEditIndex(index);
+    setEditTitle(taskArray[index].title);
+  };
+
+  const handleUpdateClick = () => {
+    if (editIndex !== null) {
+      setTaskArray((prevTasks) =>
+        prevTasks.map((task, index) =>
+          index === editIndex ? { ...task, title: editTitle } : task
+        )
+      );
+      setEditIndex(null);
+      setEditTitle("");
+    }
+  };
+
+  // Scroll to the bottom of the container when a new task is added
+  useEffect(() => {
+    if (
+      tasksContainerRef.current &&
+      taskArray.length > prevTaskLengthRef.current
+    ) {
+      tasksContainerRef.current.scrollTop =
+        tasksContainerRef.current.scrollHeight;
+    }
+    prevTaskLengthRef.current = taskArray.length;
+  }, [taskArray]);
 
   return (
     <div className="w-full h-full bg-white rounded-xl bg-opacity-95 px-10">
       <CardHeader className="border-b px-0 py-6">
         <p className="text-xl font-semibold">Create</p>
       </CardHeader>
-      <CardContent className="max-w-1/2 px-0 py-6 border-b">
+      <CardContent className="px-0 py-6 border-b">
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-8 space-x-8 flex flex-row"
+            className="space-y-8 flex flex-col"
           >
             <FormField
               control={form.control}
@@ -62,11 +114,21 @@ export default function CreatePage() {
                   <FormDescription>
                     Write down something on your mind
                   </FormDescription>
-                  <div className="flex gap-5">
-                    <FormControl>
-                      <Input {...field} placeholder="Task name" />
-                    </FormControl>
-                    <Button type="submit">Add item</Button>
+                  <div className="flex items-center gap-5 justify-between">
+                    <div className="flex flex-1 gap-5 max-w-[350px]">
+                      <FormControl>
+                        <Input {...field} placeholder="Task name" />
+                      </FormControl>
+                      <Button type="submit">Add item</Button>
+                    </div>
+                    <button
+                      type="button"
+                      onClick={deleteAll}
+                      className="flex pr-10 bg-none text-gray-400 hover:text-red-500 text-[0.9rem] "
+                    >
+                      <TrashIcon className="w-5 h-5 mx-2" />
+                      Delete all
+                    </button>
                   </div>
                   <FormMessage />
                 </FormItem>
@@ -75,23 +137,44 @@ export default function CreatePage() {
           </form>
         </Form>
       </CardContent>
-      <div className="flex flex-col py-6">
+      <div
+        className="py-6 flex-1 overflow-y-auto max-h-[60%]"
+        ref={tasksContainerRef}
+      >
+        {/* Adjust the max height based on the height of the header and any additional spacing */}
         {taskArray.map((task, index) => (
           <div
             key={index}
-            className="flex flex-row items-center justify-between rounded-lg mb-2 max-w-[350px] group hover:shadow-md py-2 px-4 space-x-3"
+            className="flex flex-row items-center justify-between rounded-lg max-w-[350px] group hover:shadow-md py-1 px-4 space-x-3"
           >
-            <div className="max-w-[300px] break-words overflow-hidden text-ellipsis">
-              {task.title}
+            <div className="flex-1">
+              {editIndex === index ? (
+                <div className="flex gap-2 items-center">
+                  <Input
+                    value={editTitle}
+                    onChange={(e) => setEditTitle(e.target.value)}
+                    className="bg-gray-200 border-none m-0 text-sm h-auto"
+                  />
+                  <Button size="xs" onClick={handleUpdateClick}>
+                    Update
+                  </Button>
+                </div>
+              ) : (
+                <div className="max-w-[300px] break-words overflow-hidden text-ellipsis text-sm">
+                  {task.title}
+                </div>
+              )}
             </div>
 
             <div className="flex flex-row space-x-2 items-center opacity-0 group-hover:opacity-100 transition-opacity">
-              <div
-                // onClick={() => editTask(index)}
-                className="cursor-pointer text-gray-400 hover:text-red-400"
-              >
-                <PencilIcon className="w-5 h-5" />
-              </div>
+              {editIndex !== index && (
+                <div
+                  onClick={() => handleEditClick(index)}
+                  className="cursor-pointer text-gray-400 hover:text-blue-400"
+                >
+                  <PencilIcon className="w-5 h-5" />
+                </div>
+              )}
               <div
                 onClick={() => deleteTask(index)}
                 className="cursor-pointer text-gray-400 hover:text-red-400"
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LIFEPLAN",
+  title: "Welcome to Lifeplan!",
   description: "Create your life!",
 };
 
