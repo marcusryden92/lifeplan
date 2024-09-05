fd7eaf9ce210d2b47c4f36f7222adaf86dff139d
marcusryden92 <marcus.ryden1992@gmail.com>
2024-09-05 15:37:34 +0200

    //

--- app/(protected)/create/circle-of-influence/page.tsx
+++ app/(protected)/create/circle-of-influence/page.tsx
@@ -89,7 +89,6 @@ export default function CapturePage() {
 
   const handleConfirmEdit = () => {
     confirmEdit({
-      taskArray,
       editIndex,
       editTitle,
       setTaskArray,
--- app/(protected)/create/mark-tasks/page.tsx
+++ app/(protected)/create/mark-tasks/page.tsx
@@ -100,7 +100,6 @@ export default function CapturePage() {
 
   const handleConfirmEdit = () => {
     confirmEdit({
-      taskArray,
       editIndex,
       editTitle,
       setTaskArray,
