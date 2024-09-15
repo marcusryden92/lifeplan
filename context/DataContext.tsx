3b5a5cbffe14784c37c6950d55b0856259c72fb4
marcusryden92 <marcus.ryden1992@gmail.com>
2024-09-15 09:38:17 +0200

    Added weekStartDay and temporary userSettings database substitute to DataContext.

--- context/DataContext.tsx
+++ context/DataContext.tsx
@@ -1,17 +1,9 @@
-"use client";
-
-import React, {
-  createContext,
-  useContext,
-  useState,
-  ReactNode,
-  useEffect,
-} from "react";
+import React, { createContext, useContext, useState, ReactNode } from "react";
 import { Planner } from "@/lib/planner-class";
-
 import { EventTemplate } from "@/utils/template-builder-utils";
 import { generateCalendar, SimpleEvent } from "@/utils/calendar-generation";
 import { templateSeed } from "@/data/template-seed";
+import { WeekStartDay } from "@/types/calendar-types";
 
 interface DataContextType {
   taskArray: Planner[];
@@ -20,21 +12,33 @@ interface DataContextType {
   setCurrentTemplate: React.Dispatch<
     React.SetStateAction<EventTemplate[] | undefined>
   >;
+  weekStartDay: WeekStartDay;
+  setWeekStartDay: React.Dispatch<React.SetStateAction<WeekStartDay>>;
 }
 
 const DataContext = createContext<DataContextType | null>(null);
 
 export const DataContextProvider = ({ children }: { children: ReactNode }) => {
+  // Database substitute:
+  const userSettings: { weekStartDay: WeekStartDay } = {
+    weekStartDay: 0,
+  };
+
   const [taskArray, setTaskArray] = useState<Planner[]>([]);
   const [currentTemplate, setCurrentTemplate] = useState<
     EventTemplate[] | undefined
   >(templateSeed);
+  const [weekStartDay, setWeekStartDay] = useState<WeekStartDay>(
+    userSettings.weekStartDay
+  );
 
   const value: DataContextType = {
     taskArray,
     setTaskArray,
     currentTemplate,
     setCurrentTemplate,
+    weekStartDay,
+    setWeekStartDay,
   };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
