d8ae7644c1743e9ce9b5cd58d751726b08ba7c21
marcusryden92 <marcus.ryden1992@gmail.com>
2024-11-20 18:24:59 +0100

    Fixed blue dragging-lines on top/bottom of item

--- components/draggable/DraggableItem.tsx
+++ components/draggable/DraggableItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useDraggableContext } from "@/context/DraggableContext";
 
 export default function DraggableItem({
@@ -8,12 +8,17 @@ export default function DraggableItem({
   children: React.ReactNode;
   taskId: string;
 }) {
+  const [mouseInhabitsTopHalf, setMouseInhabitsTopHalf] = useState<
+    boolean | null
+  >(null);
+
+  const ref = useRef<HTMLDivElement | null>(null);
+
   const {
     currentlyHoveredItem,
     setCurrentlyHoveredItem,
     currentlyClickedItem,
     setCurrentlyClickedItem,
-    isInTop,
   } = useDraggableContext();
 
   function handleMouseEnter() {
@@ -32,14 +37,34 @@ export default function DraggableItem({
     }, 10);
   }
 
+  function updateMousePosition(e: MouseEvent) {
+    if (ref.current) {
+      const rect = ref.current.getBoundingClientRect();
+      const middleY = rect.top + rect.height / 2;
+      setMouseInhabitsTopHalf(e.clientY < middleY);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      document.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  const borderClass =
+    currentlyHoveredItem === taskId &&
+    currentlyClickedItem !== taskId &&
+    currentlyClickedItem.length > 0
+      ? mouseInhabitsTopHalf
+        ? "border-t-4 border-sky-400"
+        : "border-b-4 border-sky-400"
+      : "";
+
   return (
     <div
-      id={taskId}
-      className={`${
-        currentlyHoveredItem === taskId && currentlyClickedItem !== taskId
-          ? isInTop && "border-t-4 border-sky-400"
-          : "border-b-4 border-sky-400"
-      }`}
+      ref={ref}
+      className={borderClass}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onMouseDown={handleMouseDown}
