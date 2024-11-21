82c53ef228abb3e35049dcf97657a511c63bc352
marcusryden92 <marcus.ryden1992@gmail.com>
2024-11-22 00:30:07 +0100

    Updated DraggableItem to work smoother

--- components/draggable/DraggableItem.tsx
+++ components/draggable/DraggableItem.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import { useDraggableContext } from "@/context/DraggableContext";
+import clsx from "clsx";
 
 export default function DraggableItem({
   children,
@@ -10,9 +11,9 @@ export default function DraggableItem({
   taskId: string;
   taskTitle: string;
 }) {
-  const [mouseInhabitsTopHalf, setMouseInhabitsTopHalf] =
-    useState<boolean>(false);
   const ref = useRef<HTMLDivElement | null>(null);
+  const [mouseInhabitsTopHalf, setMouseInhabitsTopHalf] = useState(false);
+  const debounceTimer = useRef<NodeJS.Timeout>();
 
   const {
     currentlyHoveredItem,
@@ -22,62 +23,72 @@ export default function DraggableItem({
     mousePosition,
   } = useDraggableContext();
 
-  // Functions to set hovered item to this item when hovering it, and clearing when leaving
-  function handleMouseEnter(event: React.MouseEvent) {
-    event.stopPropagation(); // Prevent hover propagation to parent
-    setTimeout(() => {
-      setCurrentlyHoveredItem(taskId);
-    }, 10);
-  }
-
-  function handleMouseLeave() {
-    setCurrentlyHoveredItem("");
-  }
-
-  // Function to set clicked item to this item. Clearing happens in DraggableContext
-  function handleMouseDown(event: React.MouseEvent) {
-    event.stopPropagation(); // Prevent click propagation to parent
-    setTimeout(() => {
-      setCurrentlyClickedItem({ taskId, taskTitle });
-    }, 10);
-  }
-
-  // Check if the mouse is on the bottom or top part of the div
-  useEffect(() => {
-    document.addEventListener("mousemove", updateMousePosition);
-    return () => {
-      document.removeEventListener("mousemove", updateMousePosition);
-    };
-  }, []);
-
-  function updateMousePosition() {
-    if (ref.current) {
+  // Mouse position check, with debounce for performance
+  const updateMousePosition = useCallback(() => {
+    if (
+      ref.current &&
+      currentlyClickedItem &&
+      currentlyClickedItem.taskId !== taskId
+    ) {
       const rect = ref.current.getBoundingClientRect();
       const middleY = rect.top + rect.height / 2;
       setMouseInhabitsTopHalf(mousePosition.clientY < middleY);
     }
-  }
+  }, [mousePosition, currentlyClickedItem, taskId]);
 
-  const borderClass =
-    currentlyHoveredItem === taskId &&
-    currentlyClickedItem &&
-    currentlyClickedItem.taskId !== taskId
-      ? mouseInhabitsTopHalf
-        ? "border-t-4 border-sky-400"
-        : "border-b-4 border-sky-400"
-      : "";
+  useEffect(() => {
+    clearTimeout(debounceTimer.current);
+    debounceTimer.current = setTimeout(updateMousePosition, 16); // ~60fps
+
+    return () => {
+      clearTimeout(debounceTimer.current);
+    };
+  }, [mousePosition, updateMousePosition]);
+
+  // Sets currently hovered item
+  const handleMouseEnter = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    // Timeout to prevent interference from clearing of previous item
+    setTimeout(() => {
+      setCurrentlyHoveredItem(taskId);
+    });
+  };
+
+  // Clears currently hovered item
+  const handleMouseLeave = () => {
+    setCurrentlyHoveredItem("");
+  };
+
+  // Update currently clicked item ID & title
+  // (state is cleared on mouseUp in DraggableContext)
+  const handleMouseDown = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setCurrentlyClickedItem({ taskId, taskTitle });
+  };
+
+  // Use clsx to simplify the border logic
+  const borderClasses = clsx({
+    "border-t-4 border-t-sky-400":
+      currentlyClickedItem &&
+      currentlyClickedItem?.taskId !== taskId &&
+      currentlyHoveredItem === taskId &&
+      mouseInhabitsTopHalf,
+    "border-b-4 border-b-sky-400":
+      currentlyClickedItem &&
+      currentlyClickedItem?.taskId !== taskId &&
+      currentlyHoveredItem === taskId &&
+      !mouseInhabitsTopHalf,
+  });
 
   return (
-    <>
-      <div
-        ref={ref}
-        className={`${borderClass} border `}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        onMouseDown={handleMouseDown}
-      >
-        {children}
-      </div>
-    </>
+    <div
+      ref={ref}
+      className={borderClasses}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onMouseDown={handleMouseDown}
+    >
+      {children}
+    </div>
   );
 }
