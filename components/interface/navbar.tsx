c8cf1fd9f3b25a4b1f7a1258521e5ed043c2f755
marcusryden92 <marcus.ryden1992@gmail.com>
2025-02-10 09:16:14 +0100

    Updated navbar buttons

--- components/interface/navbar.tsx
+++ components/interface/navbar.tsx
@@ -1,23 +1,54 @@
-"use client";
-
-import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { PowerIcon } from "@heroicons/react/24/outline";
-import { CogIcon } from "@heroicons/react/24/outline";
-
-import { FaPlus, FaCog, FaPowerOff, FaCalendar } from "react-icons/fa";
-import { HiTemplate } from "react-icons/hi";
-import { MdViewWeek } from "react-icons/md";
-
+import Link from "next/link";
+import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import clsx from "clsx";
-import { signOut } from "next-auth/react";
-import { Audiowide } from "next/font/google";
+import {
+  FaPlus,
+  FaCog,
+  FaPowerOff,
+  FaCalendar,
+  FaClipboardList,
+  FaFlag,
+  FaBullseye,
+} from "react-icons/fa";
+import { MdViewWeek } from "react-icons/md";
 
 const links = [
-  { name: "Calendar", href: "/calendar" },
-  { name: "Create", href: "/create" },
-  { name: "Week Template", href: "/template" },
+  {
+    name: "Calendar",
+    href: "/calendar",
+    icon: <FaCalendar className="h-5 w-5" />,
+  },
+  { name: "Create", href: "/create", icon: <FaPlus className="h-5 w-5" /> },
+  {
+    name: "Week Template",
+    href: "/template",
+    icon: <MdViewWeek className="h-5 w-5" />,
+  },
+];
+
+const createLinks = [
+  {
+    name: "Tasks",
+    href: "/create/tasks",
+    icon: <FaClipboardList className="h-5 w-5" />,
+  },
+  {
+    name: "Plans",
+    href: "/create/plans",
+    icon: <MdViewWeek className="h-5 w-5" />,
+  },
+  {
+    name: "Goals",
+    href: "/create/goals",
+    icon: <FaFlag className="h-5 w-5" />,
+  },
+  {
+    name: "Clarify Goals",
+    href: "/create/clarify-goals",
+    icon: <FaBullseye className="h-5 w-5" />,
+  },
 ];
 
 export const Navbar = () => {
@@ -25,75 +56,39 @@ export const Navbar = () => {
 
   return (
     <div className="flex lg:w-[280px] lg:h-full w-full flex-col p-2 lg:p-5">
-      <div
-        className={`hidden md:block font-audiowide text-gray-900 text-[2.2rem] mb-5`}
-      >
+      <div className="hidden md:block font-audiowide text-gray-900 text-[2.2rem] mb-5">
         LIFEPLAN
       </div>
       <div className="flex lg:h-full lg:flex-col lg:space-y-2 justify-between gap-2">
         <div className="flex lg:flex-col gap-2">
-          <Button
-            key={"calendar"}
-            asChild
-            variant={pathname === "/calendar" ? "default" : "outline"}
-            className="justify-start"
-          >
-            <Link
-              href="/calendar"
-              className={clsx(
-                "flex h-[48px] gap-2 rounded-xl p-3 text-sm font-medium hover:bg-amber-400 ",
-                { "bg-gray-800 text-white": pathname === "/calendar" }
-              )}
+          {[...links, ...createLinks].map(({ name, href, icon }) => (
+            <Button
+              key={href}
+              asChild
+              variant={pathname === href ? "default" : "outline"}
+              className="justify-start"
             >
-              <FaCalendar className="w-5 h-5" />{" "}
-              <span className="hidden lg:block">Calendar</span>
-            </Link>
-          </Button>
-          <Button
-            key="create"
-            asChild
-            variant={pathname === "/create" ? "default" : "outline"}
-            className="justify-start"
-          >
-            <Link
-              href="/create"
-              className={clsx(
-                "flex h-[48px] gap-2 rounded-xl p-3 text-sm font-medium hover:bg-amber-400 ",
-                { "bg-gray-800 text-white": pathname === "/create" }
-              )}
-            >
-              <FaPlus className="h-5 w-5" />{" "}
-              <span className="hidden lg:block">Create</span>
-            </Link>
-          </Button>
-          <Button
-            key="template"
-            asChild
-            variant={pathname === "/template" ? "default" : "outline"}
-            className="justify-start"
-          >
-            <Link
-              href="/template"
-              className={clsx(
-                "flex h-[48px] gap-2 rounded-xl p-3 text-sm font-medium hover:bg-amber-400 ",
-                { "bg-gray-800 text-white": pathname === "/template" }
-              )}
-            >
-              <MdViewWeek className="h-5 w-5" />
-              <span className="hidden lg:block">Week Template</span>
-            </Link>
-          </Button>
+              <Link
+                href={href}
+                className={clsx(
+                  "flex h-[48px] gap-2 rounded-xl p-3 text-sm font-medium hover:bg-amber-400",
+                  { "bg-gray-800 text-white": pathname === href }
+                )}
+              >
+                {icon} <span className="hidden lg:block">{name}</span>
+              </Link>
+            </Button>
+          ))}
         </div>
 
         <div className="lg:mt-auto flex lg:flex-col lg:space-y-2 content-start gap-2">
           <Button
-            key={"Setting"}
             asChild
             variant={pathname === "/settings" ? "default" : "outline"}
             className="justify-start"
           >
             <Link
-              href={"/settings"}
+              href="/settings"
               className={clsx(
                 "flex h-[48px] items-center gap-2 rounded-xl text-sm font-medium hover:bg-amber-400",
                 {
@@ -107,19 +102,13 @@ export const Navbar = () => {
             </Link>
           </Button>
           <Button
-            key={"SignOut"}
             asChild
             variant="outline"
             className="justify-start rounded-xl h-[48px] hover:text-red-500 hover:border-red-500 hover:border-2 cursor-pointer"
           >
-            <form
-              className="mt-0"
-              action={() => {
-                signOut();
-              }}
-            >
-              <button className="flex w-full  gap-2 rounded-xl text-sm font-medium hover:bg-grey-100 ">
-                <FaPowerOff className="h-5 w-5" />
+            <form className="mt-0" action={() => signOut()}>
+              <button className="flex w-full gap-2 rounded-xl text-sm font-medium hover:bg-grey-100">
+                <FaPowerOff className="h-5 w-5" />{" "}
                 <span className="hidden lg:block">Sign Out</span>
               </button>
             </form>
