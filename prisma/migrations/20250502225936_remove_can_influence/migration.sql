1f3cb78377bd074850cf4e9b38ba85db25b45955
marcusryden92 <marcus.ryden1992@gmail.com>
2025-05-03 01:01:20 +0200

    removed canInfluence column from Planner Dev Database

---
+++ prisma/migrations/20250502225936_remove_can_influence/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `canInfluence` on the `planners` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "planners" DROP COLUMN "canInfluence";
