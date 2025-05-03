3642cf0a3724e33dffe8059b30fe77190714da37
marcusryden92 <marcus.ryden1992@gmail.com>
2025-05-03 04:03:26 +0200

    moved prisma.schema to root

--- prisma/migrations/20250502225936_remove_can_influence/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `canInfluence` on the `planners` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "planners" DROP COLUMN "canInfluence";
