e70f871208c405e0bab0b662ba5cb437e08ccfaa
marcusryden92 <marcus.ryden1992@gmail.com>
2025-08-03 19:55:10 +0200

    prisma fixes

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,5 +2,5 @@ import path from "node:path";
 import type { PrismaConfig } from "prisma";
 
 export default {
-  schema: path.join("prisma"),
+  schema: path.join("prisma", "schemas"),
 } satisfies PrismaConfig;
