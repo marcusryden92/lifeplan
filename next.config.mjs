270d9132d9d74ba8685ad6325df6b3efafc2f04f
marcusryden92 <marcus.ryden1992@gmail.com>
2026-05-25 18:56:38 +0200

    Prisma fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      rrule: "rrule/dist/es5/rrule.js",
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
